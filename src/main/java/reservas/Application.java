0f3ff92e051057117ee0bf13f448168396ef8479
Javier <javier.acosta.quesada@est.una.ac.cr>
2026-09-01 19:16:58 -0600

    Pruebas de la capa logic

--- src/main/java/reservas/Application.java
+++ src/main/java/reservas/Application.java
@@ -1,9 +1,17 @@
 package reservas;
 
+import reservas.logic.Usuario;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Sistema de Reservas");
+        System.out.println("================================================");
+        System.out.println("Bienvenido al sistema de manejo de reservaciones");
+        System.out.println("================================================");
+        System.out.println("");
+        Usuario user1=new Usuario("001","001","Administrador de aparcamiento");
+        System.out.println(user1.mostrar());
+
     }
 
 }
